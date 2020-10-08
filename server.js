657c5cfe2848d5dbf51551fdeee63b71b4e6a46e
jjunyjjuny <jjuny546@gmail.com>
2020-10-08 23:29:22 +0900

    asdf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-express.static(path.join(__dirname));
+express.static(path.join(__dirname, "/"));
 app.get("/", (req, res) => {
   const index = path.join(__dirname, "index.html");
   res.sendFile(index);
