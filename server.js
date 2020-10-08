8e52fa2d1ac88f2604c7fd11e519751f68d3ca27
jjunyjjuny <jjuny546@gmail.com>
2020-10-09 03:00:23 +0900

    *

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const DIST_DIR = path.join(__dirname, "dist");
 
 app.use(express.static(DIST_DIR));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   const index = path.join(__dirname, "dist", "index.html");
   res.sendFile(index);
 });
