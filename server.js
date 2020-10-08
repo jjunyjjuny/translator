0e28ce849747d443e327ee7f4a14d60f5c626ce1
jjunyjjuny <jjuny546@gmail.com>
2020-10-08 23:42:22 +0900

    a

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
-express.static(path.join(__dirname, "/"));
+const port = process.env.PORT || 8080;
+
+const DIST_DIR = path.join(__dirname, "dist");
+
+app.use(express.static(DIST_DIR));
+
 app.get("/", (req, res) => {
-  const index = path.join(__dirname, "index.html");
+  const index = path.join(__dirname, "dist", "index.html");
   res.sendFile(index);
 });
 
-app.listen(port, () => console.log("Example"));
+app.listen(port, () => console.log("running server"));
