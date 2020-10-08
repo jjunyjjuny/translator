f0db9b51598462da515648154214c1db978f1623
jjunyjjuny <jjuny546@gmail.com>
2020-10-09 02:58:31 +0900

    not found?

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname, "dist"));
+const DIST_DIR = path.join(__dirname, "dist");
+
+app.use(express.static(DIST_DIR));
 
 app.get("/", (req, res) => {
   const index = path.join(__dirname, "dist", "index.html");
   res.sendFile(index);
 });
 
-app.listen(PORT, () => console.log("running server"));
+app.listen(PORT, () => {
+  console.log("run server");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "google_translator.js",
+  entry: "./google_translator.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
