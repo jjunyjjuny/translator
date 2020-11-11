ecea1908445d97d2d1f440b4234819a6332c659c
jjunyjjuny <jjuny546@gmail.com>
2020-11-12 04:12:03 +0900

    파파고 연결 성공, 비동기 처리 완료

--- papago_translator.js
+++ papago_translator.js
@@ -1,8 +1,8 @@
 const client_id = "b5Z06SyJtW5iQWFjwHw6";
 const client_secret = "U_qKSPfCWX";
-module.exports.papagoTranslator = (query, source, target) => {
+module.exports.papagoTranslator = async (query, source, target) => {
+  console.log("papagoTranslator on");
   const api_url = "https://openapi.naver.com/v1/papago/n2mt";
-  const request = require("request");
   const options = {
     url: api_url,
     form: { text: query, source: source, target: target },
@@ -11,19 +11,30 @@ module.exports.papagoTranslator = (query, source, target) => {
       "X-Naver-Client-Secret": client_secret,
     },
   };
-  console.log('papago 3가지', query, source, target)
+  try {
+    console.log("try on");
+    let response = await translate(options);
+    return response;
+  } catch (error) {
+    console.log(`Error at translateText --> ${error}`);
+    return "error";
+  }
+};
 
-  request.post(options, (error, response, body) => {
-    console.log('post code, error :',response.statusCode, error)
-    return new Promise((resolve, reject) => {
+const translate = async (options) => {
+  const request = require("request");
+  console.log("translate on");
+  return new Promise((resolve, reject) => {
+    request.post(options, (error, response, body) => {
+      console.log("request on");
       if (!error && response.statusCode == 200) {
+        console.log("if on");
         const result = JSON.parse(body).message.result;
-        console.log ('result :', result)
-        console.log('result.translatedText :',result.translatedText)
+        console.log("result.translatedText :", result.translatedText);
         resolve(result.translatedText);
       } else {
-        reject("error입니다용");
         console.log("error = " + response.statusCode);
+        reject(0);
       }
     });
   });
